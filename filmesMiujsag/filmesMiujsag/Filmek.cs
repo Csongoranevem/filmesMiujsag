4d67b99e3611c86858bf63e013fa00f9a5ed49d3
Csongoramevem <czakocsongor@turr.hu>
2025-01-17 10:33:23 +0100

    sec commit

---
+++ filmesMiujsag/filmesMiujsag/Filmek.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filmesMiujsag
+{
+    internal class Filmek
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private DateTime ReleaseDate { get; set; }
+        private string Length { get; set; }
+        private string Director { get; set; }
+        private string MainCharacter { get; set; }
+        private double ImdbRating{ get; set; }
+        private string Genre {  get; set; }
+
+        public Filmek(int id, string name, DateTime releaseDate, string length, string director, string mainCharacter, double imdbRating, string genre)
+        {
+            Id = id;
+            Name = name;
+            ReleaseDate = releaseDate;
+            Length = length;
+            Director = director;
+            MainCharacter = mainCharacter;
+            ImdbRating = imdbRating;
+            Genre = genre;
+        }
+
+        public Filmek(string name, DateTime releaseDate, string director, string mainCharacter, string genre)
+        {
+            Name = name;
+            ReleaseDate = releaseDate;
+            Director = director;
+            MainCharacter = mainCharacter;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. Film neve: {Name}; Megjelenés éve: {ReleaseDate.ToShortDateString()}; Játékidő: {Length} min; Rendező: {Director}; Főszereplő: {MainCharacter}; Imdb értékelés: {ImdbRating}; Műfaj: {Genre}";
+        }
+    }
+}
---
+++ filmesMiujsag/filmesMiujsag/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace filmesMiujsag
+{
+    internal class Program
+    {
+        private static string server = "localhost";
+        private static string database = "filmeses_miujsag";
+        private static string user = "root";
+        private static string password = "";
+        private static List<Filmek> filmekList = new List<Filmek>();
+
+        private static string connectionString = $"Server={server};Database={database};User ID={user};Password={password};";
+        public static MySqlConnection connection = new MySqlConnection(connectionString);
+
+
+        static void Main(string[] args)
+        {
+
+            KapcsolodasAdatbazishoz();
+            KeremAzAdatokat("movies");
+
+            Console.WriteLine(filmekList[30]);
+
+            Console.ReadKey();
+
+
+
+
+            void KapcsolodasAdatbazishoz()
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Kapcsolat sikeresen megnyitva.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Hiba a kapcsolat megnyitásakor: " + ex.Message);
+                }
+            }
+
+
+        }
+
+        private static void KeremAzAdatokat(string tableName)
+        {
+            using (MySqlCommand command = new MySqlCommand($"select * from {tableName}", connection))
+            {
+                filmekList.Clear();
+                using (MySqlDataReader mySqlDataReader = command.ExecuteReader())
+                {
+                    while (mySqlDataReader.Read())
+                    {
+                        Filmek filmBeilleszt = new Filmek(Convert.ToInt32(mySqlDataReader[0]),
+                                                         Convert.ToString(mySqlDataReader[1]),
+                                                         Convert.ToDateTime(mySqlDataReader[2]),
+                                                         Convert.ToString(mySqlDataReader[3]),
+                                                         Convert.ToString(mySqlDataReader[4]),
+                                                         Convert.ToString(mySqlDataReader[5]),
+                                                         Convert.ToDouble(mySqlDataReader[6]),
+                                                         Convert.ToString(mySqlDataReader[7]));
+
+
+                        filmekList.Add(filmBeilleszt);
+
+                    }
+                }
+            }
+        }
+    }
+}
