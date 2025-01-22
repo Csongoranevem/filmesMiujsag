5a9f8285a2341e94067f1175dac1886c17f8cbdd
Zalan12 <farkaszalan@turr.hu>
2025-01-22 10:27:42 +0100

    itt a fajl

---
+++ filmesMiujsag/filmesMiujsag/Mozik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filmesMiujsag
+{
+    internal class Mozik
+    {
+        public string Nev { get; set; }
+        public string Cim { get; set; }
+        public string Varos { get; set; }
+        public int Ferohely { get; set; }
+        public string Nyitvatart { get; set; }
+
+        public Mozik(string nev, string cim, string varos, int ferohely, string nyitvatart)
+        {
+            Nev = nev;
+            Cim = cim;
+            Varos = varos;
+            Ferohely = ferohely;
+            Nyitvatart = nyitvatart;
+        }
+        public Mozik() { }
+
+        public override string ToString()
+        {
+            return $"Mozi neve: {Nev}, mozi címe: {Cim}, városa: {Varos}, férőhelyek száma: {Ferohely}, nyitvatartás: {Nyitvatart}";
+        }
+    }
+}
--- filmesMiujsag/filmesMiujsag/Program.cs
+++ filmesMiujsag/filmesMiujsag/Program.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace filmesMiujsag
 {
     internal class Program
     {
+        static List<Mozik> moziLista = new List<Mozik>();
         private static string server = "localhost";
         private static string database = "filmeses_miujsag";
         private static string user = "root";
@@ -24,7 +26,7 @@ namespace filmesMiujsag
 
             KapcsolodasAdatbazishoz();
             KeremAzAdatokat("movies");
-
+            FileBeolvas();
             Console.WriteLine(filmekList[30]);
 
             Console.ReadKey();
@@ -46,6 +48,24 @@ namespace filmesMiujsag
             }
 
 
+        }
+
+        private static void FileBeolvas()
+        {
+            string[] adatTomb = File.ReadAllLines("fajl.txt");
+            for (int i = 0; i < adatTomb.Length; i++)
+            {
+                string nev = adatTomb[i].Split(';')[0];
+                string cim = adatTomb[i].Split(';')[1];
+                string varos = adatTomb[i].Split(';')[2];
+                int feroHely = Convert.ToInt32(adatTomb[i].Split(';')[3]);
+                string nyitvaTart = adatTomb[i].Split(';')[4];
+
+                Mozik mozi = new Mozik(nev, cim, varos, feroHely, nyitvaTart);
+                moziLista.Add(mozi);
+            }
+            Console.WriteLine(moziLista[0]);
+            Console.ReadLine();
         }
 
         private static void KeremAzAdatokat(string tableName)
